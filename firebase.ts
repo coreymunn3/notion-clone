8eba62d13fa0177c19654092277f35c53c680533
Corey Munn <michael.munn@secunetics.com>
2024-11-24 17:32:27 -0500

    export the db

--- firebase.ts
+++ firebase.ts
@@ -20,4 +20,4 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
 const analytics = getAnalytics(app);
-const db = getFirestore(app)
+export const db = getFirestore(app)
