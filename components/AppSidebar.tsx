14bf73200007c4e9a90b3675b642393428d44f47
Corey Munn <michael.munn@secunetics.com>
2024-11-24 14:39:11 -0500

    sidebar stuff

--- components/AppSidebar.tsx
+++ components/AppSidebar.tsx
@@ -4,50 +4,32 @@ import {
   SidebarGroup,
   SidebarGroupContent,
   SidebarGroupLabel,
-  SidebarInset,
-  SidebarMenu,
-  SidebarMenuButton,
-  SidebarMenuItem,
 } from "@/components/ui/sidebar";
 // icons
-import { Calendar, Home, Inbox, Search, Settings } from "lucide-react";
+import { FilePlus } from "lucide-react";
 import { Button } from "./ui/button";
 
-// Menu items.
-const items = [
-  {
-    title: "Home",
-    url: "#",
-    icon: Home,
-  },
-  {
-    title: "Inbox",
-    url: "#",
-    icon: Inbox,
-  },
-  {
-    title: "Calendar",
-    url: "#",
-    icon: Calendar,
-  },
-  {
-    title: "Search",
-    url: "#",
-    icon: Search,
-  },
-  {
-    title: "Settings",
-    url: "#",
-    icon: Settings,
-  },
-];
-
 const AppSidebar = () => {
+  const menuOptions = (
+    <>
+      <Button className="w-full">
+        {" "}
+        <FilePlus /> New Document
+      </Button>
+      <SidebarGroup>
+        <SidebarGroupLabel>My Documents</SidebarGroupLabel>
+        {/* <SidebarGroupContent></SidebarGroupContent> */}
+      </SidebarGroup>
+      <SidebarGroup>
+        <SidebarGroupLabel>Shared With Me</SidebarGroupLabel>
+        {/* <SidebarGroupContent></SidebarGroupContent> */}
+      </SidebarGroup>
+    </>
+  );
+
   return (
     <Sidebar variant="sidebar" className="z-0">
-      <SidebarContent className="p-4 mt-2">
-        <Button className="w-full">New Document</Button>
-      </SidebarContent>
+      <SidebarContent className="p-4 mt-2">{menuOptions}</SidebarContent>
     </Sidebar>
   );
 };
