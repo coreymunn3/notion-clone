18e1a3847f48ae3b4e1c4366e1854b3a3ac905ee
Corey Munn <michael.munn@secunetics.com>
2024-12-10 13:21:34 -0500

    create avatar component to show who is currently editing the doc

---
+++ components/Avatars.tsx
@@ -0,0 +1,42 @@
+"use client";
+import { useOthers, useSelf } from "@liveblocks/react/suspense";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "./ui/button";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { useEffect, useState } from "react";
+
+const Avatars = () => {
+  const users = useOthers();
+  const currentUser = useSelf();
+
+  const allUsers = [currentUser, ...users];
+
+  return (
+    <div className="flex -space-x-4">
+      {allUsers.map((user, i) => (
+        <TooltipProvider key={user.id + i}>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <Avatar className="border-2 hover:z-50">
+                <AvatarImage src={user.info.avatar} />
+                <AvatarFallback>
+                  {user.info.name?.toString()?.charAt(0)}
+                </AvatarFallback>
+              </Avatar>
+            </TooltipTrigger>
+            <TooltipContent side="right">
+              <p>{currentUser.id === user.id ? "You" : user.info.name}</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+      ))}
+    </div>
+  );
+};
+
+export default Avatars;
